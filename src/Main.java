210c75b983623e49b592990075155ee8859560b6
Brian <smilebrian0515@gmail.com>
2018-11-06 17:47:58 +0800

    Save MP3File

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 public class Main {
@@ -14,12 +16,8 @@ public class Main {
         for(File file : listOfFiles) {
             if(isMP3(file)) {
                 MP3File mp3 = new MP3File(file);
-                System.out.println(mp3.getSongName());
-                System.out.println(mp3.getArtist());
-                System.out.println(mp3.getAlbum());
-                System.out.println(mp3.getYear());
-                System.out.println(mp3.getComment());
-                System.out.println(mp3.getGenre());
+                mp3.setAlbum("阿誠");
+                mp3.save();
             }
         }
     }
@@ -27,7 +25,6 @@ public class Main {
     public static File[] getFolderFiles(String dirName) {
         File folder = new File(dirName);
         File[] listOfFiles = folder.listFiles();
-
         return listOfFiles;
     }
 
@@ -48,6 +45,7 @@ public class Main {
 }
 
 class MP3File {
+    //TAGBody 是 MP3 預留的最後128個byte，詳細資訊
     private byte[] TAGBody;
     private String Tag;
     private String SongName;
@@ -57,6 +55,21 @@ class MP3File {
     private String Comment;
     private String Genre;
     private File file;
+
+    private static final int Tag_Start = 0;
+    private static final int Tag_End = 3;
+    private static final int SongName_Start = 3;
+    private static final int SongName_End = 33;
+    private static final int Artist_Start = 33;
+    private static final int Artist_End = 63;
+    private static final int Album_Start = 63;
+    private static final int Album_End = 93;
+    private static final int Year_Start = 93;
+    private static final int Year_End = 97;
+    private static final int Comment_Start = 97;
+    private static final int Comment_End = 127;
+    private static final int Genre_Start = 127;
+    private static final int Genre_End = 128;
 
     public String getTag() {
         return Tag;
@@ -119,13 +132,13 @@ class MP3File {
             this.file = file;
             TAGBody = getTAGBody(file);
             //因為Byte轉BIG5會有位元遺失，所以先把需要的段落複製出來，再轉換成String，得到需要的結果。
-            Tag = new String(Arrays.copyOfRange(TAGBody,0,3),"BIG5").trim();
-            SongName = new String(Arrays.copyOfRange(TAGBody,3,33),"BIG5").trim();
-            Artist = new String(Arrays.copyOfRange(TAGBody,33,63),"BIG5").trim();
-            Album = new String(Arrays.copyOfRange(TAGBody,63,93),"BIG5").trim();
-            Year = new String(Arrays.copyOfRange(TAGBody,93,97),"BIG5").trim();
-            Comment = new String(Arrays.copyOfRange(TAGBody,97,127),"BIG5").trim();
-            Genre = new String(Arrays.copyOfRange(TAGBody,127,128),"BIG5").trim();
+            Tag = new String(Arrays.copyOfRange(TAGBody,Tag_Start,Tag_End),"BIG5").trim();
+            SongName = new String(Arrays.copyOfRange(TAGBody,SongName_Start,SongName_End),"BIG5").trim();
+            Artist = new String(Arrays.copyOfRange(TAGBody,Artist_Start,Artist_End),"BIG5").trim();
+            Album = new String(Arrays.copyOfRange(TAGBody,Album_Start,Album_End),"BIG5").trim();
+            Year = new String(Arrays.copyOfRange(TAGBody,Year_Start,Year_End),"BIG5").trim();
+            Comment = new String(Arrays.copyOfRange(TAGBody,Comment_Start,Comment_End),"BIG5").trim();
+            Genre = new String(Arrays.copyOfRange(TAGBody,Genre_Start,Genre_End),"BIG5").trim();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
@@ -140,6 +153,84 @@ class MP3File {
         return tagBody;
     }
 
+    public void save(){
+        try{
+            modifyTAGBody();
+            writeFile();
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
 
+    }
 
+    private void modifyTAGBody() throws Exception{
+        writeTagValueToTAGBody(SongName, SongName_Start,SongName_End);
+        writeTagValueToTAGBody(Artist, Artist_Start,Artist_End);
+        writeTagValueToTAGBody(SongName, SongName_Start,SongName_End);
+        writeTagValueToTAGBody(Album, Album_Start,Album_End);
+        writeTagValueToTAGBody(Year, Year_Start,Year_End);
+        writeTagValueToTAGBody(Comment, Comment_Start,Comment_End);
+        writeTagValueToTAGBody(Genre, Genre_Start,Genre_End);
+    }
+
+    private void writeTagValueToTAGBody(String TAG, int startIndex, int EndIndex) throws Exception {
+        byte[] b = TAG.getBytes(Charset.forName("BIG5"));
+        if(b.length > (EndIndex-startIndex)){
+            throw new Exception("MP3Value is too long to save.");
+        }else{
+            int i = 0;
+            while(startIndex < EndIndex){
+                if(i<b.length){
+                    TAGBody[startIndex++] = b[i];
+                    i++;
+                }else{
+                    TAGBody[startIndex++] = 0;
+                }
+            }
+        }
+    }
+
+    public void writeFile()throws Exception{
+        InputStream is = null;
+        OutputStream os = null;
+        try{
+            is = new FileInputStream(file);
+            os = new FileOutputStream(new File(file.getParent()+"\\..\\output\\"+file.getName()));
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = is.read(buffer)) > 0){
+                //MP3的最後128byte是TAGBody，要算好會不會斷掉。
+                if(is.available()<128 && is.available() > 0){ //取代前半段
+                    buffer=TAGBodyReplacePartOfBuffer(buffer, length,128-is.available());
+                    System.out.println("1");
+                }else{
+                    if(length < 1024){
+                        if(length >  128){ //取代整個
+                            buffer=TAGBodyReplacePartOfBuffer(buffer, length,128);
+                            System.out.println("2");
+                        }else{ //取代後半段
+                            buffer=TAGBodyReplacePartOfBuffer(buffer, length,length);
+                            System.out.println("3");
+                        }
+                    }
+                }
+
+                os.write(buffer,0,length);
+            }
+        }finally{
+            is.close();
+            os.close();
+        }
+    }
+
+    private byte[] TAGBodyReplacePartOfBuffer(byte[] buffer,int bufferLength, int replaceLength){
+        int j = 0;
+        if(buffer.length == replaceLength){
+            j = 128 - replaceLength;
+        }
+        for(int i=bufferLength-replaceLength;i<bufferLength;i++){
+            buffer[i] = TAGBody[j++];
+        }
+        return buffer;
+    }
 }
